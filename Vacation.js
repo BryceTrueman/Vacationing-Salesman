dbeeef656d4541d502bc561028f844e991c67822
Bryce Trueman <truemanbryce@yahoo.com>
2015-11-10 18:59:28 -0700

    Create Vacation.js

---
+++ Vacation.js
@@ -0,0 +1,13 @@
+var towns = new Array();
+towns[0] = first city of list
+towns[1] = second city of list
+towns[2] = third city of list
+
+var paris-nyc = 3,631.16;
+var nyc-nuuk = 1852.78;
+var paris-nuuk = 2224;
+
+if(town0 = paris && town1 = nyc)
+distance = paris-nyc
+etc...
+
